26425e7ae0e8251dd384c53807a0247ced91baef
LiuJQ9674Git <455298138@qq.com>
2021-02-26 14:25:47 +0800

    softmax-classifier

--- five_layers_relu_1.py
+++ five_layers_relu_1.py
@@ -4,16 +4,21 @@ import math
 
 
 logs_path = 'log_simple_stats_5_layers_relu_softmax'
+# 配置参数
 batch_size = 100
 learning_rate = 0.5
 training_epochs = 10
 
-
+# 数据集
 mnist = input_data.read_data_sets("MNIST_data",one_hot=True)
 
+# 输入层开始构建网络架构
+# 输入层现在是一个形状为[1×784]的张量，代表待分类图像
 X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-XT = tf.placeholder(tf.float32, [None, 28, 28, 1])
 Y_ = tf.placeholder(tf.float32, [None, 10])
+
+XT = tf.placeholder(tf.float32, [None, 28, 28, 1])
+
 lr = tf.placeholder(tf.float32)
 
 
@@ -22,23 +27,29 @@ M = 100
 N = 60
 O = 30
 
+# 网络的第一层接收待分类输入图像的像素，与W1权重连接组合，
+# 并与对应的B1偏差张量相加:
 W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  
 B1 = tf.Variable(tf.ones([L])/10)
-W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
-B2 = tf.Variable(tf.ones([M])/10)
-W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
-B3 = tf.Variable(tf.ones([N])/10)
-W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
-B4 = tf.Variable(tf.ones([O])/10)
-W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
-B5 = tf.Variable(tf.zeros([10]))
-
-
+# 第一层通过sigmoid激活函数，将自己的输出传给第二层:
 XX = tf.reshape(X, [-1, 784])
 Y1 = tf.nn.relu(tf.matmul(XX, W1) + B1)
+
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.ones([M])/10)
 Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
+
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.ones([N])/10)
 Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
+
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.ones([O])/10)
 Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
+
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
+#ReLU的运算速 度很快，因为它不需要进行指数运算，这一点与sigmoid和tanh等激活函数不同。
 Ylogits = tf.matmul(Y4, W5) + B5
 Y = tf.nn.softmax(Ylogits)
 
--- five_layers_sigmoid.py
+++ five_layers_sigmoid.py
@@ -1,55 +1,72 @@
 import tensorflow as tf
 import input_data
 import math
-# ERROR
+#
 logs_path = 'log_simple_stats_5_layers_sigmoid'
 batch_size = 100
-learning_rate = 0.5
-training_epochs = 10
+learning_rate = 0.003
+training_epochs = 100 #10 0.9442 30 0.9743 0.9781
 
 mnist = input_data.read_data_sets("MNIST_data",one_hot=True)
-X = tf.placeholder(tf.float32, [None, 28, 28, 1])
-Y_ = tf.placeholder(tf.float32, [None, 10])
 
 L = 200
 M = 100
 N = 60
 O = 30
 
-W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  
+
+# 构建网络架构
+
+# 输入层现在是一个形状为[1×784]的张量，代表待分类图像
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+XX = tf.reshape(X, [-1, 784])
+Y_ = tf.placeholder(tf.float32, [None, 10])
+# 网络的第一层接收待分类输入图像的像素，与W1权重连接组合，并与对应的B1偏差张量相加
+W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1)) #L=200
 B1 = tf.Variable(tf.zeros([L]))
-W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+# 第一层通过sigmoid激活函数，将自己的输出传给第二层:
+Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
+
+# 第二层接收第一层的输出Y1，并将其与W2权重连接组合，再加上对应的B2偏差张量:
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1)) #M=100
 B2 = tf.Variable(tf.zeros([M]))
-W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+# 第二层通过sigmoid激活函数，将输出传给第三层:
+Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
+
+# 第三层接收第二层的输出Y2，与W3权重连接组合起来，并与对应的B3偏差张量相加:
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))  #N=60
 B3 = tf.Variable(tf.zeros([N]))
-W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+# 第三层通过sigmoid激活函数，将输出传给第四层:
+Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
+
+# 第四层接收第三层的输出Y3，与W4权重连接组合起来，并与对应的B4偏差张量相加:
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1)) #O=30
 B4 = tf.Variable(tf.zeros([O]))
+# 第四层通过sigmoid激活函数，将输出传给第五层
+Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+
+# 第五层将从第四层接收O = 30的激励作为输入，这些输入会通过softmax激活函数，转化为 每个数字对应的概率:
 W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
 B5 = tf.Variable(tf.zeros([10]))
-
-
-XX = tf.reshape(X, [-1, 784])
-Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
-Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
-Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
-Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
 Ylogits = tf.matmul(Y4, W5) + B5
 Y = tf.nn.softmax(Ylogits)
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=Ylogits, logits=Y_)
+# 损失函数为，目标与softmax激活函数产生的结果之间的交叉熵cross-entropy
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
 cross_entropy = tf.reduce_mean(cross_entropy)*100
+
 correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-learning_rate = 0.003
 
-# 没有调试通过
+
+# tf.train.AdamOptimizer使用Kingma和Ba’s Adam算法控制学习率
+# AdamOptimizer比简单的tf.train.GradientDescentOptimizer有几个优势，
+# 实际上，前者使用了更大的有效更新步长，这样算法不需要经过微调即可收敛。
 train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
-tf.scalar_summary("cost", cross_entropy)
-tf.scalar_summary("accuracy", accuracy)
-summary_op = tf.merge_all_summaries()
-
-
+tf.summary.scalar("cost", cross_entropy)
+tf.summary.scalar("accuracy", accuracy)
+summary_op = tf.summary.merge_all()
 
 init = tf.initialize_all_variables()
 sess = tf.Session()
@@ -57,18 +74,22 @@ sess.run(init)
 
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
-    writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
+    #writer = tf.train.SummaryWriter(logs_path, graph=tf.get_default_graph())
+    writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
     for epoch in range(training_epochs):
         batch_count = int(mnist.train.num_examples/batch_size)
         for i in range(batch_count):
             batch_x, batch_y = mnist.train.next_batch(batch_size)
+            batch_x=batch_x.reshape(-1,28, 28, 1)
             _, summary = sess.run([train_step, summary_op],\
                                   feed_dict={X: batch_x,\
                                              Y_: batch_y})
             writer.add_summary(summary,\
                                epoch * batch_count + i)
         print("Epoch: ", epoch)
-           
-    print("Accuracy: ", accuracy.eval(feed_dict={X: mnist.test.images, Y_: mnist.test.labels}))
+    test=mnist.test.images
+    # 转型
+    test=test.reshape(-1, 28,28,1) #<class 'tuple'>: (10000, 28, 28, 1)
+    print("Accuracy: ", accuracy.eval(feed_dict={X: test, Y_: mnist.test.labels}))
     print("done")
 
